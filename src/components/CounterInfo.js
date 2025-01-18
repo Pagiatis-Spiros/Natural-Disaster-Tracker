f8662616cb95a768d30af61e680ae76f3ee87cad
Pagiatis-Spiros <std166333@ac.eap.gr>
2025-01-18 16:04:49 +0200

    Update CounterInfo.js

--- src/components/CounterInfo.js
+++ src/components/CounterInfo.js
@@ -3,16 +3,16 @@ const CounterInfo = ({ volcanoes, wildfires, storms, icebergs }) => {
     <div className="counter-info">
       <ul>
         <li>
-          Volcanoes : <strong style={{ color: "red" }}>{volcanoes}</strong>
+          Ηφαίστεια : <strong style={{ color: "red" }}>{volcanoes}</strong>
         </li>
         <li>
-          Wildfires : <strong style={{ color: "red" }}>{wildfires}</strong>
+          Πυρκαγιές : <strong style={{ color: "red" }}>{wildfires}</strong>
         </li>
         <li>
           Καταιγίδες : <strong style={{ color: "red" }}>{storms}</strong>
         </li>
         <li>
-          IceBergs : <strong style={{ color: "red" }}>{icebergs}</strong>
+          Παγόβουνα : <strong style={{ color: "red" }}>{icebergs}</strong>
         </li>
       </ul>
     </div>
